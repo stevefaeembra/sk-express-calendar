0e82eeb6c47e1cba47713f95096169c699a52b5c
Steven Kay <stevendkay@gmail.com>
2018-12-28 21:01:40 +0000

    got border styling to work on the grid

--- calendar.js
+++ calendar.js
@@ -41,10 +41,32 @@ const calendarIterator = function(startDate, days) {
     toDay += thisDay.getDate();
     let toMonth = months[thisDay.getMonth()];
     let toYear = (thisDay.getFullYear());
+
+
+    // work out css class hints
+
+    // first day of a month
     let cssClasses = "day";
     if (thisDay.getDate()=== 1) {
       cssClasses += " day--first-of-month";
+    };
+
+    // check to see if the day 1 week before this date is in a different month
+    let oneWeekBefore = new Date(thisDay);
+    oneWeekBefore.setDate(thisDay.getDate()-7);
+    if (oneWeekBefore.getMonth() != thisDay.getMonth()) {
+      cssClasses += " day--border-top";
     }
+
+    // thick borders to left of sunday and right of saturday
+    if (thisDay.getDay()==0) {
+      cssClasses += " day--border-left";
+    }
+    if (thisDay.getDay()==6) {
+      cssClasses += " day--border-right";
+    }
+
+    // add to list
     currWeek.push({
       day: toDay,
       month: toMonth,
