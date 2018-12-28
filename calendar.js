30a1a9f45fbe9267f2f375e7bb2db3ed14588887
Steven Kay <stevendkay@gmail.com>
2018-12-28 19:05:48 +0000

    cool, JS has generators

--- calendar.js
+++ calendar.js
@@ -1,21 +1,18 @@
 // calendar
 
-const calendar = function(startDate, days) {
-  console.log(startDate);
-  console.log(typeof startDate);
+const calendar = function *(startDate, days) {
   let result = [];
   let currDate = startDate;
-  console.log(currDate);
   for (let offset=0; offset < days; offset++) {
-    result.push(currDate);
+    yield currDate;
     let nextDate = new Date(currDate);
     nextDate.setDate(currDate.getDate()+1);
     currDate = nextDate;
-  }
-  return result;
+  };
 };
 
-const dates = calendar(new Date(2018,0,1), 3650);
-dates.forEach((date, index) => {
-  console.log(`${index}: ${date}`);
-})
+
+const cal = calendar(new Date(2018,0,1), 30);
+for (var day of cal) {
+  console.log(day);
+}
