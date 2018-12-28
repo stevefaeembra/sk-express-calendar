884a56f335c5539c0f942f9a1578216df2398277
Steven Kay <stevendkay@gmail.com>
2018-12-28 23:16:00 +0000

    add endpoint for a specific date

--- app.js
+++ app.js
@@ -13,14 +13,24 @@ app.use('/', express.static(publicPath));
 const port = process.env.PORT || 3000
 
 
+app.use('/date', (req,res) => {
+  const year = parseInt(req.query.year);
+  const month = parseInt(req.query.month)-1; // 1 Jan --> month 0
+  const day = parseInt(req.query.day);
+  const it = calendarIterator(new Date(year,month,day),730);
+  res.render("calendar",{data:it, message: `from ${day}/${month+1}/${year}`});
+});
+
+
 app.use('/:year', (req,res) => {
   const year = req.params["year"];
   const it = calendarIterator(new Date(year,0,1),730);
   res.render("calendar",{data:it, message: `for ${year}`});
 });
 
+
 app.use('/', (req,res) => {
-  const it = calendarIterator(new Date(),730);
+  const it = calendarIterator(new Date(2019,0,1),730);
   res.render("calendar",{data:it, message: "from today"});
 });
 
