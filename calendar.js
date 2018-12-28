6ceec71ad45650e1dd9a9413164e119b114a8ad3
Steven Kay <stevendkay@gmail.com>
2018-12-28 19:29:09 +0000

    simple grid, simplified by rounding to start of week 1 (sunday)

--- calendar.js
+++ calendar.js
@@ -2,7 +2,10 @@
 
 const calendar = function *(startDate, days) {
   let result = [];
-  let currDate = startDate;
+  // change it to go back to start of week (0=sun)
+  let startDate2 = new Date(startDate);
+  startDate2.setDate(startDate.getDate()-startDate.getDay());
+  let currDate = startDate2;
   for (let offset=0; offset < days; offset++) {
     yield currDate;
     let nextDate = new Date(currDate);
@@ -12,7 +15,18 @@ const calendar = function *(startDate, days) {
 };
 
 
-const cal = calendar(new Date(2018,0,1), 30);
+const cal = calendar(new Date(), 90);
+let week = 0;
+console.log("Su Mo Tu We Th Fr Sa");
+let weekString="";
 for (var day of cal) {
-  console.log(day);
+  if (day.getDay()===0) {
+    console.log(weekString);
+    weekString="";
+    weekString += (day.getDate()<10) ? "0" : "";
+    weekString += day.getDate() + " ";
+  } else {
+    weekString += (day.getDate()<10) ? "0" : "";
+    weekString += day.getDate() + " ";
+  }
 }
